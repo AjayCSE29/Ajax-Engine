8f3a5f185f9ea209c5eb10d6d1cf8aee6e988fa1
Ajay <110125104016@aalimec.ac.in>
2026-03-03 17:03:08 +0530

    Update main.cpp
    1.    Bug fixes. (And spelling mistakes)
    2.    Wrapped everything with class and thus increased the reusability of the codes.
    3.    Implemented Bresenham line algorithm.
    4.    Drawn a 2D triangle without any pixel loss (pixel error) {Successful implementation of the Bresenham Line Algorithm}
    5.    Made the code more dynamic ensuring successful implementation of Dynamic memory allocation and data safety management.

--- main.cpp
+++ main.cpp
@@ -20,119 +20,218 @@ take place on a daily basis so look forward to the code, thank you.
  And csdint library which gives fixed-size integer types like uint32_t and uint8_t
  which is important for pixel math (uint32_t and uint8_t represents an unsigned
  integer with an exact size of 32 bits (8 bits))*/
-#include<SDL2/SDL.h>
+
 #include<iostream>
-#include<csdint>
+#include<SDL2/SDL.h>
+#include<cstdint>
 
 const int WIDTH = 800; //Width of the window
 const int HEIGHT = 600; //Height of the window
 
-int main() { //The SDL_Init(SDL_INIT_VIDEO) starts the SDL video subsystem
+class Engine {
+  public:
+    bool init();
+    void run();
+    void cleanup();
+    
+  private:
+    void handleEvents();
+    void update();
+    void render();
+    void drawPixel(int x, int y, uint32_t color);
+    void drawLine(int x1, int y1, int x2, int y2, uint32_t color);
+    void drawTriangle(int x1, int y1, int z1,
+                      int x2, int y2, int z2,
+                      int x3, int y3, int z3,
+                      uint32_t color);
+    
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+    SDL_Texture* texture = nullptr;
+    uint32_t* pixels = nullptr;
+    
+    bool running = false;
+    int time = 0;
+};
+
+bool Engine::init() { //The SDL_Init(SDL_INIT_VIDEO) starts the SDL video subsystem
   if(SDL_Init(SDL_INIT_VIDEO) != 0) {
-    std::cout<<"SDL Init Error: "<<SDL_GetError() << std::endl;
+    std::cout<<"SDL Init Error: "<< SDL_GetError() << std::endl;
     return 1; //SDL_GetErroe() says the reason of error if one occurs
   }
   
-  std::cout<<"Window created"<<std::endl;
-  
-  SDL_Window* window = SDL_CreateWindow( //Creates OS level window
-    "SDL Test", //Window title
-    SDL_WINDOWPOS_CENTERED, //Position of window (X-axis)
+  window = SDL_CreateWindow(
+  //Creates OS level window
+    "Ajax Engine",
+    SDL_WINDOWPOS_CENTERED,  //Position of window (X-axis)
     SDL_WINDOWPOS_CENTERED, //Position of window (Y-axis)
     WIDTH, HEIGHT,
     SDL_WINDOW_SHOWN //Shows the window on the screen
   );
-
- //The below code block is to ensure that window is available, otherwise it closes
+  
+   //The below code block is to ensure that window is available, otherwise it closes
   if(!window) {
-    std::cout<<"Window Error: "<< SDL_GetError << std::endl;
-    SDL_Quit();
+    std::cout<<"Window Error: " << SDL_GetError() << std::endl;
     return 1;
   }
   
-  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  /*Creates a renderer (A drawing engine attached to the window)
+  renderer = SDL_CreateRenderer(
+    window,
+    -1,
+    SDL_RENDERER_ACCELERATED
+  );
+  
+   /*Creates a renderer (A drawing engine attached to the window)
   {window: Which window to draw on, -1: automatically chooses GPU, 
    SDL_RENDERER_ACCELERATED: Use hardware acceleration}
    -> These allows GPU Based drawing  */
-
+  
   //The below code block is to ensure that renderer is available, otherwise it closes
   if(!renderer){
-    std::cout << "Renderer error: "<<SDL_GetError() << std::endl;
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    std::cout<<"Renderer error: " << SDL_GetError() << std::endl;
     return 1;
   }
-
- //This creates a GPU texture
- SDL_Texture*texture = SDL_CreateTexture(
+  
+  texture = SDL_CreateTexture(
+  //This creates a GPU texture
     renderer,
-    SDL_PIXELFORMAT_ARGB8888, //Imporrant parameter, each pixel is R + G + B + A = 32 bits in total (8 bits each)
-    SDL_TEXTUREACCESS_STREAMING, //Ensures that this texture updates every frame form the CPU
+    SDL_PIXELFORMAT_RGBA8888, //Imporrant parameter, each pixel is R + G + B + A = 32 bits in total (8 bits each)
+    SDL_TEXTUREACCESS_STREAMING,  //Ensures that this texture updates every frame form the CPU
     WIDTH, HEIGHT
   );
-
- //The below code block is to ensure that texture is available, otherwise it closes
-  if(!texture) {
-    std::cout<<"Texture Error: "<< SDL_GetError() << std::endl;
+  
+   //The below code block is to ensure that texture is available, otherwise it closes
+  if(!texture){
+    std::cout<<"Texture Error: " << SDL_GetError() << std::endl;
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 1;
   }
-
-  uint32_t* pixels = new uint32_t[WIDTH * HEIGHT]; //This creates a dynamic block of memory, also called as framebuffer
-                                                   //800 * 600 = 480,000 pixels, each pixel if 32 bits (4 bytes), the total memory used is ~1.8 MB
- 
-  bool running = true; //Controls loop, ensures the program doesn't exit immediately when executed 
-  SDL_Event event; //Its a structure that stores input events such keyboard, mouse, window close etc
-  int time = 0;
   
-  while(running) { //Ensure the engine is running until it's closed
-    while(SDL_PollEvent(&event)) { //Checks for events
-      if(event.type == SDL_QUIT){
-        running = false;
-      }
-    }
-    //Initiating pixel drawing	
-    for(int y = 0; y < HEIGHT; y++){ //Loop through rows
-      for(int x = 0; x < WIDTH; x++) { //Loop through columns
-        int index = y * WIDTH + x; //Converting 2D into 1D index because memory is a 1D array
-							//Color calculation, each channel ranges from 0-255
-							//The colors are based on x, y position and time for animation 
-        uint32_t r = (x + time) % 256; //256 ensures that values are inside color range
-        uint32_t g = (y + time) % 256; 
-        uint32_t b = (x + y + time) % 256;
-        
-        pixels[index] = (255 << 24) | (r << 16) | (g << 8) | (b << 8); //Packing RGBA into 32 bits
-																								//The animation is acheived by bit-shifting between r, g, b and a (Alpha)
-																								//These are combined usinf a bitwise operator {Final layout in memory: [R][G][B][A]}
-      }
-    }
-    time++; //This ensures that for each frame, color shifts slightly, the reason gradient moves
-   
-    //The below code is from the previous version to check if the created window can draw or not, feel free to use this to check
-    /*SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //Creates a empty / Black window
-    SDL_RenderClear(renderer); //Fills the entire screen with current color (Black)
-    SDL_Rect rect = {300, 200, 200, 150}; //Creates a rectangle to draw
-                    //position: x, y and width, height
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); //Sets drawing color to red
-    SDL_RenderFillRect(renderer, &rect); //Draws a rectangle */
-     
-    SDL_UpdateTexture(texture, NULL, pixels, WIDTH * sizeof(uint32_t)); //VERY IMPORTANT: This copies the CPU frame buffer to GPU texture, the screen stays black without it
-    SDL_RenderClear(renderer); //Clears renderer
-    SDL_RenderCopy(renderer, texture, NULL, NULL); //Copies full texture onto screen
-    SDL_RenderPresent(renderer); //Swaps buffers
-  }
-
-  delete[] pixels;
-  //Memory cleanup (Dynamic)
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  //Close window
-  SDL_Quit();
-  
-  std::cout<<"Window Destroyed"<<std::endl;
-  return 0;
+  pixels = new uint32_t[WIDTH * HEIGHT];  //This creates a dynamic block of memory, also called as framebuffer
+                                           //800 * 600 = 480,000 pixels, each pixel if 32 bits (4 bytes), the total memory used is ~1.
+  running = true; //Controls loop, ensures the program doesn't exit immediately when executed 
+  return true;
 }
 
+void Engine::handleEvents(){
+ //Its a structure that stores input events such keyboard, mouse, window close etc
+  SDL_Event event;
+    while(SDL_PollEvent(&event)) { //Checks for events
+      if(event.type == SDL_QUIT) {
+        running = false;
+    }
+  }
+}
+
+void Engine::update(){
+  time++;  //This the frame changes to time
+}
+
+void Engine::drawPixel(int x, int y, uint32_t color){
+  if(x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT){
+    return;
+  }
+  pixels[y * WIDTH + x] = color;
+}
+
+void Engine::drawLine(int x1, int y1, int x2, int y2, uint32_t color){
+  int dx = abs(x2 - x1);
+  int dy = abs(y2 - y1);
+  
+  int sx = (x1 < x2) ? 1 : -1;
+  int sy = (y1 < y2) ? 1 : -1;
+  int err = dx - dy;
+  while(true){
+    drawPixel(x1, y1, color);
+    if(x1 == x2 && y1 == y2){
+      break;
+    }
+    int e2 = 2 * err;
+    if(e2 > -dy){
+      err -= dy;
+      x1 += sx;
+    }
+     if(e2 < dx) {
+      err += dx;
+      y1 += sy;
+     }
+  }
+}
+
+void Engine::drawTriangle(int x1, int y1, int z1,
+                          int x2, int y2, int z2,
+                          int x3, int y3, int z3,
+                          uint32_t color){
+  drawLine(x1, y1, x2, y2, color);
+  drawLine(x2, y2, x3, y3, color);
+  drawLine(x3, y3, x1, y1, color);
+}
+
+uint32_t white = 0xFFFFFFFF;
+void Engine::render(){
+
+  for(int i = 0; i < WIDTH * HEIGHT; i++){
+    pixels[i] = 0x000000FF;
+  }
+  
+  drawTriangle(
+    400, 150, 0,
+    200, 450, 0,
+    600, 450, 0,
+    white
+  );
+  
+  /* TEST FOR DRAWLINE
+  drawLine(100, 100, 700, 500, white);
+  drawLine(700, 100, 100, 500, white);
+  drawLine(400, 0, 400, 600, white);
+  drawLine(0, 300, 800, 300, white); */
+
+ /* TEST FOR SDL2
+ for(int y = 0; y < HEIGHT; y++){
+    for(int x = 0; x < WIDTH; x++){
+      int index = y * WIDTH + x;
+      uint32_t r = (x + time) % 256;
+      uint32_t g = (y + time) % 256;
+      uint32_t b = (x + y + time) % 256;
+      
+      pixels[index] = 
+        (r << 24) |
+        (g << 16) |
+        (b << 8) |
+        255;
+    }
+  } */   
+  
+  SDL_UpdateTexture(texture, NULL, pixels, WIDTH * sizeof(uint32_t)); //VERY IMPORTANT: This copies the CPU frame buffer to GPU texture, the screen stays black without it
+  SDL_RenderClear(renderer); //Clears renderer
+  SDL_RenderCopy(renderer, texture, NULL, NULL); //Copies full texture onto screen
+  SDL_RenderPresent(renderer); //Swaps buffers
+}
+
+void Engine::run(){
+  while(running){
+    handleEvents();
+    update();
+    render();
+  }
+}
+
+void Engine::cleanup(){
+  delete[] pixels;
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
+
+int main(){
+  Engine obj;
+  if(!obj.init()){
+    return 1;
+  }
+  obj.run();
+  obj.cleanup();
+}
